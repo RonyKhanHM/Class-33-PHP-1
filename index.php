c75c777ccbefff724261482d1feaf01e37318092
RonyKhanHM <Rajakhanhmr@gmail.com>
2024-06-26 22:24:50 +0600

    This is php class-1

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h3>This is HTML</h3>
+</body>
+
+</html>
+
+
+<?php
+  echo "<h2>php html<h2>";
+  echo "Hello World <br>";
+
+  $number1 = 10;
+  $number2 = 20;
+
+  $result =  $number1 + $number2;
+  echo "The rusult is=". $result.'<br>';
+
+  for ($i = 0; $i < 5; $i++){
+  // echo "Hello World <br>";
+  echo $i."<br>";
+  }
+
+?>
